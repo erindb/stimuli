d1bffc7298aa20c36f70675b1f9d49f29bc92fc3
fredcallaway <fredc@llaway.com>
2017-05-01 17:06:16 -0700

    don't add unique() to array prototype
    it's bad form (and can cause bugs) to change the built-in
    prototypes

--- ecosystem.js
+++ ecosystem.js
@@ -1,12 +1,5 @@
 function rm(v, item) {if (v.indexOf(item) > -1) { v.splice(v.indexOf(item), 1); }}
 
-Array.prototype.unique = function() {
-    var o = {}, i, l = this.length, r = [];
-    for(i=0; i<l;i+=1) o[this[i]] = this[i];
-    for(i in o) r.push(o[i]);
-    return r;
-};
-
 var ColorRandomizer = function(nStep) {
     function shuffle(v) { newarray = v.slice(0);for(var j, x, i = newarray.length; i; j = parseInt(Math.random() * i), x = newarray[--i], newarray[i] = newarray[j], newarray[j] = x);return newarray;} // non-destructive.
     var nSteps = nSteps==null ? 10 : nSteps;
